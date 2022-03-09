d7ac526f6eef7afe0cef2865cb1b353f0ca93b02
SaifN97 <saifnarpali@gmail.com>
2022-03-09 17:03:11 +0530

    Date picker

--- components/Header.js
+++ components/Header.js
@@ -6,12 +6,36 @@ import {
   UserCircleIcon,
   UsersIcon,
 } from '@heroicons/react/outline'
+import 'react-date-range/dist/styles.css' // main style file
+import 'react-date-range/dist/theme/default.css' // theme css fileimport 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css' // theme css filez
+import { DateRangePicker } from 'react-date-range'
+import { useState } from 'react'
 
 const Header = () => {
+  const [searchInput, setSearchInput] = useState('')
+  const [numberOfGuests, setNumberOfGuests] = useState(1)
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate)
+    setEndDate(ranges.selection.endDate)
+  }
+
+  const selectionRange = {
+    startDate,
+    endDate,
+    key: 'selection',
+  }
+
+  const resetInput = (e) => {
+    setSearchInput('')
+  }
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-md md:px-10">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white p-5 shadow-md md:px-10">
       {/* Left section */}
-      <div className="relative flex items-center h-10 my-auto cursor-pointer">
+      <div className="relative my-auto flex h-10 cursor-pointer items-center">
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -20,24 +44,56 @@ const Header = () => {
         />
       </div>
       {/* Mid section (search input) */}
-      <div className="flex items-center py-2 rounded-full md:border-2 md:shadow-sm">
+      <div className="flex items-center rounded-full py-2 md:border-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           placeholder="Start your search"
-          className="flex-grow pl-5 text-sm text-gray-600 bg-transparent outline-none placeholder:text-gray-400"
+          className="flex-grow bg-transparent pl-5 text-sm text-gray-600 outline-none placeholder:text-gray-400"
         />
-        <SearchIcon className="hidden h-8 p-2 text-white bg-red-400 rounded-full cursor-pointer md:mx-2 md:inline-flex" />
+        <SearchIcon className="hidden h-8 cursor-pointer rounded-full bg-red-400 p-2 text-white md:mx-2 md:inline-flex" />
       </div>
 
       {/* Right section */}
       <div className="flex items-center justify-end space-x-4 text-gray-500">
         <p className="hidden cursor-pointer md:inline">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
-        <div className="flex items-center p-2 space-x-2 border-2 rounded-full">
+        <div className="flex items-center space-x-2 rounded-full border-2 p-2">
           <MenuIcon className="h-6" />
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="col-span-3 mx-auto flex flex-col">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#fd5b61']}
+            onChange={handleSelect}
+          />
+          <div className="mb-4 flex items-center border-b">
+            <h2 className="flex-grow text-2xl font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              value={numberOfGuests}
+              onChange={(e) => setNumberOfGuests(e.target.value)}
+              min={1}
+              className="w-12 pl-2 text-lg text-red-400 outline-none"
+            />
+          </div>
+          <div className="flex">
+            <button className="flex-grow text-gray-500" onClick={resetInput}>
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   )
 }
