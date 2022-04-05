c2b16eea5a40aa08bc3a9e2ebd67217c5defcb9c
SaifN97 <saifnarpali@gmail.com>
2022-04-05 16:35:51 +0530

    Added map

---
+++ components/Map.js
@@ -0,0 +1,63 @@
+import { getCenter } from 'geolib'
+import { useState } from 'react'
+import ReactMapGL, { Marker, Popup } from 'react-map-gl'
+
+const Map = ({ searchResults }) => {
+  const [selectedLocation, setSelectedLocation] = useState({})
+  const coords = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }))
+
+  const center = getCenter(coords)
+
+  const [viewport, setViewport] = useState({
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+    width: '100%',
+    height: '100%',
+  })
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapStyle="mapbox://styles/saifn97/cl1kp98zz000a15p2vjg8w84y"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      width="100%"
+      height="100%"
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className="animate-bounce cursor-pointer text-2xl"
+              aria-label="push-pin"
+            >
+              📍
+            </p>
+          </Marker>
+
+          {selectedLocation.long === result.long && (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  )
+}
+
+export default Map
--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
+  env: {
+    mapbox_key:
+      'pk.eyJ1Ijoic2FpZm45NyIsImEiOiJjbDFrcDJvc3owMnhrM2dyejZydTJtcTdzIn0.XvlGtfgZVaSEIJwrENlFzw',
+  },
 }
